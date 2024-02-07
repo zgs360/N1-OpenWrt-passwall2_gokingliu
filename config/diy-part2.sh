0571a35ff3ecb221b71a631aa1749b7bd119354e
AJ <983097239@qq.com>
2024-02-07 16:21:59 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -21,8 +21,8 @@ git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-alidd
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 # => passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # => ssr plus
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
