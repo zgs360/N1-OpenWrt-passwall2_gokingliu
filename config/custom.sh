cfc5b8b4923d8910f4345c39938e24ca8b00fd8c
zgs360 <zgs360@gmail.com>
2025-08-31 18:40:26 +0800

    Update custom.sh

--- config/custom.sh
+++ config/custom.sh
@@ -2,7 +2,7 @@
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.128.10.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
 
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
@@ -10,3 +10,5 @@ git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclas
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 
+#修改密码
+sed -i 's/^root:.*:/root:$1$sKnYlDLr$7fZvROo6z5iKO9FIYGEbf0:18248:0:99999:7:::/g' package/base-files/files/etc/shadow
